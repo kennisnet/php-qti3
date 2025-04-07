cc690f2f22938b5cc99cfd4c0509031616f158e5
Karel Visser <k.visser@kennisnet.nl>
2025-04-07 13:54:25 +0200

    SOLIT-910 Removed IFileUtils

--- src/Infrastructure/Filesystem/FileSystemUtils.php
+++ src/Infrastructure/Filesystem/FileSystemUtils.php
@@ -8,7 +8,7 @@ use Exception;
 use RuntimeException;
 use SplFileObject;
 
-class FileSystemUtils implements IFileSystemUtils
+class FileSystemUtils
 {
     public function generateTempFilename(): string
     {
