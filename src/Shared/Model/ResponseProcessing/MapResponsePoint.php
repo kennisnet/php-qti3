cef7995d8f673c43a6d38e95f320d8209f651848
Karel Visser <k.visser@kennisnet.nl>
2025-05-08 09:04:31 +0200

    SOLIT-940 Fixed issue with response processing of select point interactions. Added Equal and MapResponsePoint models and updated HotspotAssessmentItemNormalizer for proportional scoring`

---
+++ src/Shared/Model/ResponseProcessing/MapResponsePoint.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\SharedKernel\Domain\Qti\Shared\Model\ResponseProcessing;
+
+use App\SharedKernel\Domain\Qti\Shared\Model\Processing\INumericExpression;
+use App\SharedKernel\Domain\Qti\Shared\Model\QtiElement;
+
+class MapResponsePoint extends QtiElement implements INumericExpression
+{
+    public function __construct(
+        public readonly string $identifier,
+    ) {}
+
+    public function attributes(): array
+    {
+        return [
+            'identifier' => $this->identifier,
+        ];
+    }
+}
