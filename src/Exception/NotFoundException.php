16b5b013399ecdec47c6874fe1e60120dc8a97c6
Mark van der Feijst <m.vanderfeijst@kennisnet.nl>
2026-01-02 14:18:44 +0100

    FLEX-498 Add Resource manager and voter in preparation of editor

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace App\SharedKernel\Domain\Exception;
 
+use Throwable;
+
 final class NotFoundException extends DomainError
 {
     public function __construct(
         private readonly string $customMessage,
         private readonly string $errorCode,
+        private readonly ?Throwable $previous = null,
     ) {
-        parent::__construct($this->errorMessage());
+        parent::__construct($this->errorMessage(), previous: $this->previous);
     }
 
     public function errorCode(): string
